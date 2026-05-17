6de649e8ed800b60179ab1c487f14fb4d06c357f
Mohammad Rezaee <mohammadrezaee22@gmail.com>
2026-05-17 11:32:23 +0200

    the bug is fixed in AccessContorlServiceProvider

--- src/modules/AccessControl/Providers/AccessControlServiceProvider.php
+++ src/modules/AccessControl/Providers/AccessControlServiceProvider.php
@@ -9,6 +9,7 @@ use App\Modules\AccessControl\Models\Permission;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -32,12 +33,16 @@ class AccessControlServiceProvider extends ServiceProvider{
 
         //using the Laravel's gate gives us the possbility to use Blade's helper or middelwares
         //to introduce all the permissiions to Laravel so we can use its CAN method to see if the user has that permission.
+        if(Schema::hasTable('permissions')){
+            //we check if the table is ready, before the migration it will make error
             Permission::all()->map(function($permission){
                 //beacuse we call the CAN mehtod on a user, so we have the user here
                 Gate::define($permission->name, function ($user) use ($permission) {
                     return $user->hasPermission($permission);
                 });
             });
+        }
+
 
             Blade::if('role',function($role){
                return auth()->check() && auth()->user()->hasRole($role);
